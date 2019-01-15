5bba8ecea4c4a2532de57176ce046ec7544e4cbc
jkjnmlk <123@example.com>
2019-01-15 11:44:48 +0800

    构建基本结构框架

--- main.c
+++ main.c
@@ -2,7 +2,38 @@
 
 int main(){
 
-    printf("hello world!");
+    printf("进入存钱取钱管理系统\n");
+      while (1)
+    {
+        int code = 0;
+        printf("1--存钱\n");
+        printf("2--取钱\n");
+        printf("3--余额查询\n");
+        printf("4--退出\n");
+        printf("请输入所需项目符号\n");
+        scanf("%d", &code);
+
+        
+        if (code==1) {
+            /* code */
+        }
+        
+        if (code==2) {
+            /* code */
+        }
+        
+        if (code==3) {
+            /* code */
+        }
+        
+        if (code==4) {
+            /* code */
+        }
+        
+        
+        
+        
+    }
 
     return 0;
 
